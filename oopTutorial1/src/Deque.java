b178a1853353fab7dea48404075552630e25e7de
078e9 <jb723505@gmail.com>
2022-12-09 08:30:32 +0600

    202221209_083045

---
+++ oopTutorial1/src/Deque.java
@@ -0,0 +1,25 @@
+/*
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<Integer> myDeque = new LinkedList<Integer>();
+
+        myDeque.add(1); // Добавляем последний элемент
+        myDeque.addFirst(2); // Добавляем элемент в начало
+        myDeque.addLast(3); // Добавляем элемент в конец
+        myDeque.push(4); // Добавляем в начало
+        myDeque.offer(5); // Добавляем в конец
+        myDeque.offerFirst(6); // Добавляем в начало
+
+        System.out.println(myDeque + "\n"); // Выводим
+
+        // Удаляем первый и посдений элементы
+        myDeque.removeFirst();
+        myDeque.removeLast();
+
+        // Выводим данные после удаления
+        System.out.println("Данные после удаления: " + myDeque);
+    }
+}
+ */
--- oopTutorial1/src/Main.java
+++ oopTutorial1/src/Main.java
@@ -2,46 +2,27 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set FirstSet = new TreeSet();
-        List FirstList = new LinkedList();
+        Queue<String> myEnemies = new ArrayDeque<>();
 
-        FirstSet.add(2);
-        FirstSet.add(1);
-        FirstSet.add(3);
+        myEnemies.add("Kem Daniil");
+        myEnemies.add("Secret Person 1582");
+        myEnemies.add("Secret Person 0785");
+        myEnemies.add("Secret Person 0957");
+        myEnemies.add("Secret Person 0107");
 
-        FirstSet.add(5);
-        FirstSet.add(5);
-        FirstSet.add(5);
+        System.out.println("Мой первый враг: " + ((ArrayDeque<String>) myEnemies).peekFirst());
+        System.out.println("Мой последний враг: " + ((ArrayDeque<String>) myEnemies).peekLast());
+        System.out.println("Мой последний враг после удаления: " + ((ArrayDeque<String>) myEnemies).removeLast());
+        System.out.println("Мой первый враг после удаления: " + ((ArrayDeque<String>) myEnemies).removeFirst());
+        System.out.println("Все мои враги умерли? Ответ: " + ((ArrayDeque<String>) myEnemies).isEmpty());
 
-        FirstSet.add(new Integer(25));
-        FirstSet.add(new Integer(31));
-        FirstSet.add(new Integer(14));
-
-        System.out.println(FirstSet);
-
-        Map map = new HashMap();
-
-        // Заполнем его чем-нибудь
-        map.put("one", "111");
-        map.put("two", "222");
-        map.put("three", "333");
-        map.put("four", "333");
-
-        // Получаем и выводим значения
-        System.out.println("Set of keys: " + map.keySet());
-
-        // Получаем и выводим значения по ключу
-        String val = (String)map.get("one");
-        System.out.println("one=" + val);
-
-        // Получить и вывести все значения
-        System.out.println("Collection of values: " + map.values());
-
-        // Получить и вывести все пары значений и ключей
-        System.out.println("Set of entries: " + map.entrySet());
+        System.out.println(myEnemies);
 
     }
 }
+
+
+
 
 
 
