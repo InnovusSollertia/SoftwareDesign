2a767c874b9b05c3be961066c4abbe94063426ee
ndrw0 <jb723505@gmail.com>
2023-04-22 16:38:57 +0600

    20230422_162112

---
+++ HW_STM/F407DHT1/Lcd f407/MDK-ARM/Lcd f407/dht.d
@@ -0,0 +1,3 @@
+lcd f407\dht.o: ..\Src\DHT.c
+lcd f407\dht.o: ../Inc/DHT.h
+lcd f407\dht.o: ../Inc/main.h
---
+++ HW_STM/F407DHT1/f407_lcd/MDK-ARM/f407_lcd/dht.d
@@ -0,0 +1,3 @@
+f407_lcd\dht.o: ..\Src\DHT.c
+f407_lcd\dht.o: ../Inc/DHT.h
+f407_lcd\dht.o: ../Inc/main.h
