8446c4f022d9bc5294f9f1a4d8f08c4c849b6d9a
InnovusSollertia <jb723505@gmail.com>
2023-09-21 17:09:39 +0600

    20230921_1643

--- PL_Python/ds/main.py
+++ PL_Python/ds/main.py
@@ -1,4 +1,13 @@
 import pandas as pd 
 import numpy as np
 
-print('Hello world!')
+a_list = list("abcdefg")
+numpy_array = np.arange(1, 10)
+dictionary = {"A":  0, "B":1, "C":2, "D":3, "E":5}
+
+series1 = pd.Series(a_list)
+print(series1)
+series2 = pd.Series(numpy_array)
+print(series2)
+series3 = pd.Series(dictionary)
+print(series3)
